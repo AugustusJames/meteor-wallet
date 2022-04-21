ace9367c82a435ed14be1b924ce7bc39256910b1
Amber Westmoreland <amberwestmoreland@Ambers-MacBook-Pro.local>
2022-04-21 08:17:51 -0600

    First commit

---
+++ imports/ui/ContactForm.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { ContactsCollection } from "../api/ContactsCollection";
+
+
+
+export const ContactForm = () => {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [imageUrl, setImageUrl] = React.useState("");
+
+    const saveContact = () => {
+       // console.log({ name, email, imageUrl });
+        ContactsCollection.insert({ name, email, imageUrl });
+        setName("");
+        setEmail("");
+        setImageUrl("");
+    }
+
+    return (
+        <form>
+      <div>
+        <label htmlFor="name">
+          Name 
+        </label>
+        <input 
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"/>
+      </div>
+      <div>
+        <label htmlFor="email">
+          Email
+        </label>
+        <input 
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"/>
+      </div>
+      <div>
+        <label htmlFor="imageUrl">
+          Image URL
+        </label>
+        <input 
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            type="text"/>
+      </div>
+      <div>
+        <button type="button" onClick={saveContact}>Save Contact</button>
+      </div>
+    </form>
+    )
+}
